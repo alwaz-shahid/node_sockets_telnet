45789661a9a03590d8a3a77585fc83cd402e2478
Alwaz18 <alwazk8@gmail.com>
2022-03-01 20:25:17 +0500

    udp

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
-import net from "net";
+// import net from "net";
 // import { EventEmitter } from "events";
 
-const server = net.createServer((socket) => {
-  socket.write("hello world\n");
-  socket.on("data", (data) => {
-    console.log(data.toString());
-  });
-});
-server.listen(8080);
+// const server = net.createServer((socket) => {
+//   socket.write("hello world\n");
+//   socket.on("data", (data) => {
+//     console.log(data.toString());
+//   });
+// });
+// server.listen(8080);
 
 // telnet 127.0.0.1 8080
+
+import dgram from "dgram";
+
+const server = dgram.createSocket("udp4");
+
+server.on("error", (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on("message", (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on("listening", () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(8081);
+// Prints: server listening 0.0.0.0:8081
+// echo "hi" | nc -w1 -u 127.0.0.1 8081
